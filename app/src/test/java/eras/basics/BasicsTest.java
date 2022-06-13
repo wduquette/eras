3dd09bebb9a24b6973872cec2ee896f1643332e4
Will Duquette <will@wjduquette.com>
2022-06-13 09:36:52 -0700

    Added some tests.

--- app/src/test/java/eras/basics/BasicsTest.java
+++ app/src/test/java/eras/basics/BasicsTest.java
@@ -2,6 +2,9 @@ package eras.basics;
 
 import eras.Ted;
 import org.junit.Test;
+
+import java.util.List;
+
 import static eras.checker.Checker.*;
 
 public class BasicsTest extends Ted {
@@ -16,5 +19,18 @@ public class BasicsTest extends Ted {
 
         require(Basics.lengthOfYearElementInDays(withLeapDay, 2019), eq(28));
         require(Basics.lengthOfYearElementInDays(withLeapDay, 2020), eq(29));
+    }
+
+    @Test public void testLengthOfYearInDays() {
+        title("testLengthOfYearInDays");
+
+        YearElement normalMonth = new YearElement.Month("June", 30);
+        YearElement withLeapDay = new YearElement.MonthWithLeapDay(
+            "February", 28, new LeapRule.Gregorian());
+        YearlyCycle cycle = new YearlyCycle(List.of(withLeapDay, normalMonth));
+
+        require(Basics.lengthOfYearInDays(cycle, 2019), eq(58));
+        require(Basics.lengthOfYearInDays(cycle, 2020), eq(59));
+
     }
 }
